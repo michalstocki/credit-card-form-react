4d5b7f670320b67089fe581dd753a0f1cdf3941c
Michał Stocki <michal.stocki@uxpin.com>
2020-07-22 15:25:13 +0200

    Integration tests for formatting expiry date

--- src/components/CreditCardForm/__tests__/CreditCardForm.test.tsx
+++ src/components/CreditCardForm/__tests__/CreditCardForm.test.tsx
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import React from 'react';
 import { CreditCardForm } from '../../../index';
+import { INPUT_ERROR_CLASS } from '../components/Input/Input';
 
 describe('CreditCardForm', () => {
   it('renders Card Input', () => {
@@ -49,6 +50,84 @@ describe('CreditCardForm', () => {
         expect(
           (document.activeElement as HTMLInputElement).placeholder
         ).toEqual('MM / YY');
+      });
+    });
+  });
+
+  describe('when typed past expiration date in expiry input', () => {
+    let expiryInput: HTMLInputElement;
+    let cvcInput: HTMLInputElement;
+
+    beforeEach(() => {
+      const { getByPlaceholderText } = render(<CreditCardForm />);
+
+      expiryInput = getByPlaceholderText('MM / YY') as HTMLInputElement;
+      cvcInput = getByPlaceholderText('CVC') as HTMLInputElement;
+
+      expiryInput.focus();
+      fireEvent.input(expiryInput, {
+        target: { value: '1211' },
+      });
+    });
+
+    it('correctly formats the expiry date', async () => {
+      await waitFor(() => {
+        expect(expiryInput.value).toEqual('12 / 11');
+      });
+    });
+
+    it('colors the input text', () => {
+      expect(expiryInput).toHaveClass(INPUT_ERROR_CLASS);
+    });
+
+    it('provides error message as an input title', () => {
+      expect(expiryInput).toHaveAttribute('title', 'Your card has expired');
+    });
+
+    it("doesn't move focus to the next input", async () => {
+      await waitFor(() => {
+        expect(
+          (document.activeElement as HTMLInputElement).placeholder
+        ).toEqual('MM / YY');
+      });
+    });
+  });
+
+  describe('when typed correct expiration date in expiry input', () => {
+    let expiryInput: HTMLInputElement;
+    let cvcInput: HTMLInputElement;
+
+    beforeEach(() => {
+      const { getByPlaceholderText } = render(<CreditCardForm />);
+
+      expiryInput = getByPlaceholderText('MM / YY') as HTMLInputElement;
+      cvcInput = getByPlaceholderText('CVC') as HTMLInputElement;
+
+      expiryInput.focus();
+      fireEvent.input(expiryInput, {
+        target: { value: '0530' },
+      });
+    });
+
+    it('correctly formats the expiry date', async () => {
+      await waitFor(() => {
+        expect(expiryInput.value).toEqual('05 / 30');
+      });
+    });
+
+    it("doesn't change color the input text", async () => {
+      expect(expiryInput).not.toHaveClass(INPUT_ERROR_CLASS);
+    });
+
+    it("doesn't set any title attribute", () => {
+      expect(expiryInput).not.toHaveAttribute('title', expect.any(String));
+    });
+
+    it('moves focus to the next input', async () => {
+      await waitFor(() => {
+        expect(
+          (document.activeElement as HTMLInputElement).placeholder
+        ).toEqual('CVC');
       });
     });
   });
